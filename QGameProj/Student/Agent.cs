f172482011b19707e173fd893915229c072905b3
Hamdi-Almahfali <hamdi.almahfali@gmail.com>
2024-11-02 20:35:37 +0100

    Cleaned up agent class

--- QGameProj/Student/Agent.cs
+++ QGameProj/Student/Agent.cs
@@ -26,21 +26,6 @@ class Agent : BaseAgent
         return graph.FindRoute(bräde);
     }
 
-
-    private bool IsValidMove(Node current, Node next, SpelBräde bräde)
-    {
-        if (next.Position.X < 0 || next.Position.X >= 8 || next.Position.Y < 0 || next.Position.Y >= 8)
-        {
-            return false;
-        }
-        /// ------------->>> this is for when we implement wall checks <<<<---------------------
-        //if (bräde.IsWallBetween(current, next))
-        //{
-        //    return false;
-        //}
-        return true;
-    }
-
     public override Drag GörOmDrag(SpelBräde bräde, Drag drag)
     {
         System.Diagnostics.Debugger.Break();
